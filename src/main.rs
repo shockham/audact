c0404b5144c829c384f915bf869938bed0b3e92b
shockham <samuel.hockham@gmail.com>
2017-06-05 20:34:12 +0100

    Outputs bass

--- src/main.rs
+++ src/main.rs
@@ -7,78 +7,61 @@ use futures::task::Executor;
 use futures::task::Run;
 
 use std::sync::Arc;
-use std::thread;
-use std::time::Duration;
 
-use cpal::{ Endpoint, Format, EventLoop };
+struct AudactExecutor;
 
-struct MyExecutor;
-
-impl Executor for MyExecutor {
+impl Executor for AudactExecutor {
     fn execute(&self, r: Run) {
         r.run();
     }
 }
 
-struct Audact {
-    endpoint: Endpoint,
-    format: Format,
-    event_loop: EventLoop,
-}
-
 fn main() {
     let endpoint = cpal::get_default_endpoint().expect("Failed to get default endpoint");
     let format = endpoint.get_supported_formats_list().unwrap().next().expect("Failed to get endpoint format");
+    let event_loop = cpal::EventLoop::new();
 
-    let audact = Audact {
-        endpoint: endpoint,
-        format: format,
-        event_loop: cpal::EventLoop::new(),
-    };
+    {
+        let executor = Arc::new(AudactExecutor);
+        let (mut voice, stream) = cpal::Voice::new(&endpoint, &format,
+                                                   &event_loop).expect("Failed to create a voice");
 
-    let task = loop_channel(&audact);
+        let freq = 100.0;
+        //let gap = Duration::from_millis(250);
 
-    audact.event_loop.run();
-}
+        // Produce a sinusoid of maximum amplitude.
+        let samples_rate = format.samples_rate.0 as f32;
+        let mut data_source = (0u64..).map(move |t| t as f32 * freq * 2.0 * 3.141592 / samples_rate)     // 440 Hz
+            .map(move |t| t.sin());
 
-fn loop_channel(audact:&Audact) {
-    let executor = Arc::new(MyExecutor);
-    let (mut voice, stream) = cpal::Voice::new(&audact.endpoint, &audact.format,
-                                               &audact.event_loop).expect("Failed to create a voice");
+        voice.play();
 
-    let freq = 100.0;
-    let gap = Duration::from_millis(250);
+        let task = stream.for_each(move |buffer| -> Result<_, ()> {
+            match buffer {
+                cpal::UnknownTypeBuffer::U16(mut buffer) => {
+                    for (sample, value) in buffer.chunks_mut(format.channels.len()).zip(&mut data_source) {
+                        let value = ((value * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
+                        for out in sample.iter_mut() { *out = value; }
+                    }
+                },
+                cpal::UnknownTypeBuffer::I16(mut buffer) => {
+                    for (sample, value) in buffer.chunks_mut(format.channels.len()).zip(&mut data_source) {
+                        let value = (value * std::i16::MAX as f32) as i16;
+                        for out in sample.iter_mut() { *out = value; }
+                    }
+                },
+                cpal::UnknownTypeBuffer::F32(mut buffer) => {
+                    for (sample, value) in buffer.chunks_mut(format.channels.len()).zip(&mut data_source) {
+                        for out in sample.iter_mut() { *out = value; }
+                    }
+                },
+            };
 
-    // Produce a sinusoid of maximum amplitude.
-    let samples_rate = audact.format.samples_rate.0 as f32;
-    let mut data_source = (0u64..).map(move |t| t as f32 * freq * 2.0 * 3.141592 / samples_rate)     // 440 Hz
-        .map(move |t| t.sin());
+            Ok(())
+        });
+        
+        task::spawn(task).execute(executor);
+    }
 
-    voice.play();
-
-    let task = stream.for_each(move |buffer| -> Result<_, ()> {
-        match buffer {
-            cpal::UnknownTypeBuffer::U16(mut buffer) => {
-                for (sample, value) in buffer.chunks_mut(audact.format.channels.len()).zip(&mut data_source) {
-                    let value = ((value * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
-                    for out in sample.iter_mut() { *out = value; }
-                }
-            },
-            cpal::UnknownTypeBuffer::I16(mut buffer) => {
-                for (sample, value) in buffer.chunks_mut(audact.format.channels.len()).zip(&mut data_source) {
-                    let value = (value * std::i16::MAX as f32) as i16;
-                    for out in sample.iter_mut() { *out = value; }
-                }
-            },
-            cpal::UnknownTypeBuffer::F32(mut buffer) => {
-                for (sample, value) in buffer.chunks_mut(audact.format.channels.len()).zip(&mut data_source) {
-                    for out in sample.iter_mut() { *out = value; }
-                }
-            },
-        };
-
-        Ok(())
-    });
-    
-    task::spawn(task).execute(executor);
+    event_loop.run();
 }
