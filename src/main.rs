92034a6a15ce41603895b66fc4a9728107a3bb8d
shockham <samuel.hockham@gmail.com>
2017-06-06 17:02:01 +0100

    Added square wave and ability to choose wave type

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,42 @@ use cpal::{ UnknownTypeBuffer, Endpoint, EventLoop, Voice };
 
 use std::sync::Arc;
 
+enum Wave {
+    Sine,
+    Square,
+}
+
 fn main() {
     let endpoint = cpal::get_default_endpoint().expect("Failed to get default endpoint");
     let event_loop = Arc::new(EventLoop::new());
 
-    voice_channel(&endpoint,  &event_loop, 100.0).play();
-    voice_channel(&endpoint,  &event_loop, 200.0).play();
+    voice_channel(&endpoint,  &event_loop, 100.0, Wave::Sine).play();
+    voice_channel(&endpoint,  &event_loop, 200.0, Wave::Square).play();
 
     (*event_loop).run();
 }
 
-fn voice_channel(endpoint: &Endpoint, event_loop: &Arc<EventLoop>, freq: f32) -> Voice {
+fn sine_wave(t:f32) -> f32 {
+    t.sin()
+}
+
+fn square_wave(t:f32) -> f32 {
+    t.sin().round()
+}
+
+fn voice_channel(endpoint: &Endpoint, event_loop: &Arc<EventLoop>, freq: f32, wave: Wave) -> Voice {
     let format = endpoint.get_supported_formats_list().unwrap().next().expect("Failed to get endpoint format");
     let (voice, stream) = cpal::Voice::new(&endpoint, &format,
                                                &event_loop).expect("Failed to create a voice");
 
+    let wave = match wave {
+        Wave::Sine => sine_wave,
+        Wave::Square => square_wave,
+    };
+
     let samples_rate = format.samples_rate.0 as f32;
     let mut data_source = (0u64..).map(move |t| t as f32 * freq * 2.0 * 3.141592 / samples_rate)
-        .map(move |t| t.sin());
+        .map(wave);
 
     let task = stream.for_each(move |buffer| -> Result<_, ()> {
         match buffer {
