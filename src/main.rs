267a837c10056d583c0281edb6c75b04bb4762d1
shockham <samuel.hockham@gmail.com>
2017-06-06 14:02:13 +0100

    Refactored voice creation out into separate fn

--- src/main.rs
+++ src/main.rs
@@ -6,52 +6,53 @@ use futures::task;
 use futures::task::Executor;
 use futures::task::Run;
 
-use cpal::UnknownTypeBuffer;
+use cpal::{ UnknownTypeBuffer, Endpoint, EventLoop, Voice };
 
 use std::sync::Arc;
 
-struct AudactExecutor;
-
-impl Executor for AudactExecutor {
-    fn execute(&self, r: Run) {
-        r.run();
-    }
-}
-
 fn main() {
     let endpoint = cpal::get_default_endpoint().expect("Failed to get default endpoint");
+    let event_loop = Arc::new(EventLoop::new());
+
+    voice_channel(&endpoint,  &event_loop).play();
+
+    (*event_loop).run();
+}
+
+fn voice_channel(endpoint: &Endpoint, event_loop: &Arc<EventLoop>) -> Voice {
     let format = endpoint.get_supported_formats_list().unwrap().next().expect("Failed to get endpoint format");
-    let event_loop = cpal::EventLoop::new();
+    let (voice, stream) = cpal::Voice::new(&endpoint, &format,
+                                               &event_loop).expect("Failed to create a voice");
+
+    let freq = 100.0;
+    //let gap = Duration::from_millis(250);
+
+    let samples_rate = format.samples_rate.0 as f32;
+    let mut data_source = (0u64..).map(move |t| t as f32 * freq * 2.0 * 3.141592 / samples_rate)
+        .map(move |t| t.sin());
+
+    let task = stream.for_each(move |buffer| -> Result<_, ()> {
+        match buffer {
+            UnknownTypeBuffer::F32(mut buffer) => {
+                for (out, value) in buffer.iter_mut().zip(&mut data_source) {
+                    *out = value;
+                }
+            },
+            _ => (),
+        };
+
+        Ok(())
+    });
 
     {
-        let executor = Arc::new(AudactExecutor);
-        let (mut voice, stream) = cpal::Voice::new(&endpoint, &format,
-                                                   &event_loop).expect("Failed to create a voice");
-
-        let freq = 100.0;
-        //let gap = Duration::from_millis(250);
-
-        let samples_rate = format.samples_rate.0 as f32;
-        let mut data_source = (0u64..).map(move |t| t as f32 * freq * 2.0 * 3.141592 / samples_rate)
-            .map(move |t| t.sin());
-
-        voice.play();
-
-        let task = stream.for_each(move |buffer| -> Result<_, ()> {
-            match buffer {
-                UnknownTypeBuffer::F32(mut buffer) => {
-                    for (out, value) in buffer.iter_mut().zip(&mut data_source) {
-                        *out = value;
-                    }
-                },
-                _ => (),
-            };
-
-            Ok(())
-        });
-        
-        task::spawn(task).execute(executor);
+        struct AudactExecutor;
+        impl Executor for AudactExecutor {
+            fn execute(&self, r: Run) {
+                r.run();
+            }
+        }
+        task::spawn(task).execute(Arc::new(AudactExecutor));
     }
 
-    event_loop.run();
+    voice
 }
