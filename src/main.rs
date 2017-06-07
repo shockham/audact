9b2a197af6598867d22a43c1cc7faba17b36ea14
shockham <samuel.hockham@gmail.com>
2017-06-07 20:21:21 +0100

    Move functions into the Audact impl

--- src/main.rs
+++ src/main.rs
@@ -17,37 +17,36 @@ enum Wave {
     Square,
 }
 
-
 struct Audact {
     endpoint: Endpoint,
     event_loop: Arc<EventLoop>,
+    voice_channels: Vec<Voice>,
 }
 
 fn main() {
-    let endpoint = cpal::get_default_endpoint().expect("Failed to get default endpoint");
-    let event_loop = Arc::new(EventLoop::new());
+    let mut audact = Audact::new();
 
-    let audact = Audact {
-        endpoint: endpoint,
-        event_loop: event_loop,
-    };
+    audact.voice_channel(100.0, Wave::Sine).unwrap();
+    audact.voice_channel(200.0, Wave::Square).unwrap();
 
-    let mut voice_steps = vec![
-        (voice_channel(&audact.endpoint,  &audact.event_loop, 100.0, Wave::Sine), [0,4,8,12]),
-        (voice_channel(&audact.endpoint,  &audact.event_loop, 200.0, Wave::Square), [2,6,10,14]),
+    let sequences = vec![
+        [0,4,8,12],
+        [2,6,10,14],
     ];
 
     let bpm_duration = Duration::from_millis(500); // 120 bpm
+
+    let mut tmp_voice_channels = audact.voice_channels;
 
     thread::spawn(move || {
         loop {
             // simple 16-step sequencer
             for step in 0 .. 16 {
-                for i in 0 .. voice_steps.len() {
-                    if let Ok(_) = voice_steps[i].1.binary_search(&step) {
-                        voice_steps[i].0.play();
+                for i in 0 .. tmp_voice_channels.len() {
+                    if let Ok(_) = sequences[i].binary_search(&step) {
+                        tmp_voice_channels[i].play();
                     } else {
-                        voice_steps[i].0.pause();
+                        tmp_voice_channels[i].pause();
                     }
                 }
                 thread::sleep(bpm_duration);
@@ -66,42 +65,58 @@ fn square_wave(t:f32) -> f32 {
     t.sin().round()
 }
 
-fn voice_channel(endpoint: &Endpoint, event_loop: &Arc<EventLoop>, freq: f32, wave: Wave) -> Voice {
-    let format = endpoint.get_supported_formats_list().unwrap().next().expect("Failed to get endpoint format");
-    let (voice, stream) = cpal::Voice::new(&endpoint, &format,
-                                               &event_loop).expect("Failed to create a voice");
+impl Audact {
+    fn new() -> Audact {
+        let endpoint = cpal::get_default_endpoint().expect("Failed to get default endpoint");
+        let event_loop = Arc::new(EventLoop::new());
 
-    let wave = match wave {
-        Wave::Sine => sine_wave,
-        Wave::Square => square_wave,
-    };
-
-    let samples_rate = format.samples_rate.0 as f32;
-    let mut data_source = (0u64..).map(move |t| t as f32 * freq * 2.0 * 3.141592 / samples_rate)
-        .map(wave);
-
-    let task = stream.for_each(move |buffer| -> Result<_, ()> {
-        match buffer {
-            UnknownTypeBuffer::F32(mut buffer) => {
-                for (out, value) in buffer.iter_mut().zip(&mut data_source) {
-                    *out = value;
-                }
-            },
-            _ => (),
-        };
-
-        Ok(())
-    });
-
-    {
-        struct AudactExecutor;
-        impl Executor for AudactExecutor {
-            fn execute(&self, r: Run) {
-                r.run();
-            }
+        Audact {
+            endpoint: endpoint,
+            event_loop: event_loop,
+            voice_channels: Vec::new(),
         }
-        task::spawn(task).execute(Arc::new(AudactExecutor));
     }
 
-    voice
+    fn voice_channel(&mut self, freq: f32, wave: Wave) -> Result<bool, bool> {
+        let format = self.endpoint.get_supported_formats_list()
+            .unwrap().next().expect("Failed to get endpoint format");
+        let (voice, stream) = cpal::Voice::new(&self.endpoint, &format,
+                                                   &self.event_loop).expect("Failed to create a voice");
+
+        let wave = match wave {
+            Wave::Sine => sine_wave,
+            Wave::Square => square_wave,
+        };
+
+        let samples_rate = format.samples_rate.0 as f32;
+        let mut data_source = (0u64..).map(move |t| t as f32 * freq * 2.0 * 3.141592 / samples_rate)
+            .map(wave);
+
+        let task = stream.for_each(move |buffer| -> Result<_, ()> {
+            match buffer {
+                UnknownTypeBuffer::F32(mut buffer) => {
+                    for (out, value) in buffer.iter_mut().zip(&mut data_source) {
+                        *out = value;
+                    }
+                },
+                _ => (),
+            };
+
+            Ok(())
+        });
+
+        {
+            struct AudactExecutor;
+            impl Executor for AudactExecutor {
+                fn execute(&self, r: Run) {
+                    r.run();
+                }
+            }
+            task::spawn(task).execute(Arc::new(AudactExecutor));
+        }
+
+        self.voice_channels.push(voice);
+
+        Ok(true)
+    }
 }
