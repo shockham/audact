1b2b3e30b21aac40c399175406cd296709682c55
shockham <samuel.hockham@gmail.com>
2017-06-28 15:35:03 +0100

    Publish 0.3.0

--- examples/main.rs
+++ examples/main.rs
@@ -7,9 +7,9 @@ fn main() {
     let mut audact = Audact::new(16, 100, 4f32);
 
     //lead
-    audact.voice_channel(notes::C3, Wave::Saw, (0f32, 1f32), vec![0,4,8,12]).unwrap();
-    audact.voice_channel(notes::D3, Wave::Saw, (0f32, 1f32), vec![1,5,9,13]).unwrap();
-    audact.voice_channel(notes::F3, Wave::Saw, (0f32, 1f32), vec![2,3,6,7,10,11,14,15]).unwrap();
+    audact.voice_channel(notes::C3, Wave::Saw, (0f32, 0.8f32), vec![0,4,8,12]).unwrap();
+    audact.voice_channel(notes::D3, Wave::Saw, (0f32, 0.8f32), vec![1,5,9,13]).unwrap();
+    audact.voice_channel(notes::F3, Wave::Saw, (0f32, 0.8f32), vec![2,3,6,7,10,11,14,15]).unwrap();
 
     //pad
     audact.voice_channel(notes::C3, Wave::Square, (0f32, 1f32), vec![0,1,2,3,4,5,6,7]).unwrap();
