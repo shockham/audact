4f671084608eb598c3fec008e1837b4559447f56
shockham <samuel.hockham@gmail.com>
2017-07-25 14:13:11 +0100

    Add patterns example

---
+++ examples/patterns.rs
@@ -0,0 +1,22 @@
+extern crate audact;
+
+use audact::system::{ Audact, Wave };
+use audact::notes::std_note_freq;
+
+fn main() {
+    let mut pattern_1 = Audact::new(16, 100, 4f32);
+
+    // single test tone
+    pattern_1.channel(std_note_freq(-12), Wave::Sine,
+                   0.7f32, (0f32, 1f32), vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]).unwrap();
+
+    Audact::start(pattern_1, 1);
+    
+    let mut pattern_2 = Audact::new(16, 100, 4f32);
+
+    // single test tone
+    pattern_2.channel(std_note_freq(-10), Wave::Sine,
+                   0.7f32, (0f32, 1f32), vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]).unwrap();
+
+    Audact::start(pattern_2, 1);
+}
