d44d9118ba466d94e6169ad76c498d24c82d941c
shockham <samuel.hockham@gmail.com>
2018-06-06 10:16:09 +0100

    Fix so no small gaps in playback by generating all the samples ahead of time

--- src/system.rs
+++ src/system.rs
@@ -33,15 +33,13 @@ pub struct Audact {
     /// Sample rate
     sample_rate: u32,
     /// Samples needed per step
-    samples_needed: f32,
+    total_samples_needed: f32,
 }
 
 /// Stuct to represent a channel
 struct Channel {
     /// The Sink that the channel plays from
     sink: Sink,
-    /// The sequence that the channel plays
-    seq: Vec<i32>,
     /// The samples the channel plays
     source: Vec<f32>,
     /// Processing for the channel
@@ -68,20 +66,21 @@ impl Audact {
     pub fn new(steps: i32, bpm: i32, per_bar: f32) -> Audact {
         let endpoint = rodio::default_output_device().unwrap();
         // Sample rate and step duration
-        let samples_rate = 44100f32;
+        let sample_rate = 44100f32;
         let bpm_duration =
             Duration::from_millis((((60f32 / bpm as f32) * 1000f32) / per_bar) as u64);
         // Calculate the number of samples needed per step
         let subsecs = bpm_duration.subsec_nanos() as f32 / 100_000_000f32;
         let samples_needed =
-            samples_rate * ((bpm_duration.as_secs() as f32 + subsecs) / 4f32) * 0.8f32;
+            sample_rate * ((bpm_duration.as_secs() as f32 + subsecs) / 4f32) * 0.8f32;
+        let total_samples_needed = samples_needed * steps as f32;
         // Create and return instance
         Audact {
             endpoint,
             channels: Vec::new(),
             steps,
-            sample_rate: samples_rate as u32,
-            samples_needed,
+            sample_rate: sample_rate as u32,
+            total_samples_needed,
         }
     }
 
@@ -119,13 +118,21 @@ impl Audact {
         sink.pause();
         sink.set_volume(volume);
 
-        let samples_rate = self.sample_rate as f32;
+        let sample_rate = self.sample_rate as f32;
+        let b_seq = seq.clone();
+        let steps = self.steps as f32;
+        let total_samples_needed = self.total_samples_needed;
 
         // Create the basic waveform samples
-        let source: Vec<f32> = (0u64..self.samples_needed as u64)
+        let source: Vec<f32> = (0u64..self.total_samples_needed as u64)
             .map(move |t| {
+                // Silence if not playing in this step
+                let s_t = total_samples_needed / t as f32;
+                if !b_seq.contains(&((steps / s_t).floor() as i32)) {
+                    return 0f32;
+                }
                 // Calc the freq for the wave
-                let freq = t as f32 * freq * PI / samples_rate;
+                let freq = t as f32 * freq * PI / sample_rate;
                 // Call the wave gen fn
                 match wave {
                     Wave::Sine => Audact::sine_wave(freq),
@@ -139,7 +146,6 @@ impl Audact {
         // Create the processing chain and channel
         let channel = Channel {
             sink,
-            seq,
             source,
             processing,
         };
@@ -150,35 +156,26 @@ impl Audact {
     /// Kick off audact to start and loop 'bars' times
     pub fn start(&mut self, bars: i32) {
         // grab some values from the stuct to be moved
-        let steps = self.steps;
-        //let bpm_duration = self.bpm_duration;
         let tmp_voice_channels = &self.channels;
         let sample_rate = self.sample_rate;
-        let samples_needed = self.samples_needed as usize;
         // The repeats of the sequence
         for _ in 0..bars {
-            // simple step sequencer
-            for step in 0..steps {
-                for chan in tmp_voice_channels {
-                    // Check if the channel is triggered this step and get source samples or silence
-                    let samples = if chan.seq.binary_search(&step).is_ok() {
-                        chan.source.clone()
-                    } else {
-                        vec![0f32; samples_needed]
-                    };
-                    // create buffer
-                    let sample_buffer = vec![SamplesBuffer::new(2, sample_rate, samples)];
-                    // create the source
-                    let source = source::from_iter(sample_buffer)
-                        .fade_in(chan.processing.attack)
-                        .low_pass(chan.processing.filter.1 as u32)
-                        .amplify(chan.processing.gain);
-                    // add source to sink queue
-                    chan.sink.append(source);
-                    // Play the channel
-                    chan.sink.play();
-                }
+            for chan in tmp_voice_channels {
+                // create buffer
+                let samples = chan.source.clone();
+                let sample_buffer = vec![SamplesBuffer::new(2, sample_rate, samples)];
+                // create the source
+                let source = source::from_iter(sample_buffer)
+                    .fade_in(chan.processing.attack)
+                    .low_pass(chan.processing.filter.1 as u32)
+                    .amplify(chan.processing.gain);
+                // add source to sink queue
+                chan.sink.append(source);
             }
+        }
+        // Play all the channels
+        for chan in tmp_voice_channels {
+            chan.sink.play();
         }
         // Sleep until the end of the sequence
         for chan in tmp_voice_channels {
